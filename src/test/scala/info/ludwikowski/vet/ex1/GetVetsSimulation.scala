f532d5471b3b4b03838b394a70b44eb0cadd8ce1
Andrzej Ludwikowski <andrzejludwikowski@gmail.com>
2019-09-18 07:19:25 +0200

    ex1 solution

---
+++ src/test/scala/info/ludwikowski/vet/ex1/GetVetsSimulation.scala
@@ -0,0 +1,22 @@
+package info.ludwikowski.vet.ex1
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class GetVetsSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:9966/petclinic") // Here is the root for all relative URLs
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+
+  val scn = scenario("Exercise 1")
+    .exec(http("get vets")
+      .get("/api/vets")
+      .check(status.is(200)))
+
+
+  setUp(scn.inject(atOnceUsers(10)).protocols(httpProtocol))
+}

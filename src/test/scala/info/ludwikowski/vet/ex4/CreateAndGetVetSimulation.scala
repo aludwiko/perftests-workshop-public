91f56e40584c50888b49872930dce52ec7f2fb44
Andrzej Ludwikowski <andrzejludwikowski@gmail.com>
2019-09-19 07:35:51 +0200

    ex4 solution

---
+++ src/test/scala/info/ludwikowski/vet/ex4/CreateAndGetVetSimulation.scala
@@ -0,0 +1,59 @@
+package info.ludwikowski.vet.ex4
+
+import info.ludwikowski.base.PetClinicSimulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class CreateAndGetVetSimulation extends PetClinicSimulation {
+
+  val scn = scenario("Exercise 2")
+    .exec(http("create vet")
+      .post("/api/vets")
+      .body(StringBody(
+        """
+          |{
+          |  "id":null,
+          |  "firstName":"Antoni",
+          |  "lastName":"Ludwikowski",
+          |  "specialties":[
+          |    {
+          |      "id":1,
+          |      "name":"radiology"
+          |    }
+          |  ]
+          |}
+          |""".stripMargin)).asJson
+      .check(status.is(201))
+      .check(jsonPath("$..id").exists.saveAs("vetId"))
+      .check(jsonPath("$..firstName").saveAs("vetFirstName"))
+      .check(jsonPath("$..lastName").saveAs("vetLastName"))
+    )
+    .exec(http("update speciality")
+      .put("/api/vets/${vetId}")
+      .body(StringBody(
+        """
+          |{
+          |  "id":${vetId},
+          |  "firstName":"${vetFirstName}",
+          |  "lastName":"${vetLastName}",
+          |  "specialties":[
+          |    {
+          |      "id":1,
+          |      "name":"radiology"
+          |    },
+          |    {
+          |      "id":3,
+          |      "name":"dentistry"
+          |    }
+          |  ]
+          |}
+          |""".stripMargin)).asJson
+      .check(status.is(204))
+    )
+    .exec(http("get wet")
+      .get("/api/vets/${vetId}")
+      .check(jsonPath("$..specialties[*].name").findAll.is(Seq("dentistry","radiology")))
+    )
+
+  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+}

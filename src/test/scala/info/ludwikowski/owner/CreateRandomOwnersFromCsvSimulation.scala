e976d367b085758a0e2c2b9789985beb0133ef82
Andrzej Ludwikowski <andrzejludwikowski@gmail.com>
2019-09-21 20:28:26 +0200

    csv feeder

--- src/test/scala/info/ludwikowski/owner/CreateRandomOwnersFromCsvSimulation.scala
+++ src/test/scala/info/ludwikowski/owner/CreateRandomOwnersFromCsvSimulation.scala
@@ -4,27 +4,15 @@ import info.ludwikowski.base.PetClinicSimulation
 import info.ludwikowski.owner.OwnerActions.createOwner
 import io.gatling.core.Predef._
 
-import scala.util.Random
-
-class CreateRandomOwnersSimulation extends PetClinicSimulation {
-
-  val cities = List("Wrocław", "Warszawa", "Zielona Góra")
+class CreateRandomOwnersFromCsvSimulation extends PetClinicSimulation {
 
   private val ownerIdKey = "ownerId"
   private val firstNameKey = "firstName"
   private val lastNameKey = "lastName"
   private val cityKey = "city"
 
-  val randomOwners = Iterator.continually(
-    Map(
-      firstNameKey -> s"Andrzej_${Random.alphanumeric.take(4).mkString}",
-      lastNameKey -> s"Ludwikowski_${Random.alphanumeric.take(4).mkString}",
-      cityKey -> cities(Random.nextInt(cities.size))
-    )
-  )
-
   val scn = scenario("Exercise 9")
-    .feed(randomOwners)
+    .feed(csv("owners_data.csv").circular)
     .exec(createOwner(ownerIdKey, firstNameKey, lastNameKey, cityKey))
 
   setUp(scn.inject(atOnceUsers(5)).protocols(httpProtocol))
--- src/test/scala/info/ludwikowski/owner/OwnerActions.scala
+++ src/test/scala/info/ludwikowski/owner/OwnerActions.scala
@@ -1,6 +1,5 @@
 package info.ludwikowski.owner
 
-import info.ludwikowski.base.GatlingElUtils
 import info.ludwikowski.base.GatlingElUtils.toEL
 import io.gatling.core.Predef.{StringBody, jsonPath, _}
 import io.gatling.http.Predef.{http, status, _}
@@ -39,6 +38,23 @@ object OwnerActions {
     .check(status.is(201))
     .check(jsonPath("$..id").exists.saveAs(ownerIdKey))
 
+  def createOwner(ownerIdKey: String, firstNameKey: String, lastNameKey: String, cityKey: String) =
+    http("create owner")
+      .post("/api/owners")
+      .body(StringBody(
+        s"""
+          |{
+          |  "id":null,
+          |  "firstName":"${toEL(firstNameKey)}",
+          |  "lastName":"${toEL(lastNameKey)}",
+          |  "address":"address",
+          |  "city":"${toEL(cityKey)}",
+          |  "telephone":"123123123"
+          |}
+          |""".stripMargin)).asJson
+      .check(status.is(201))
+      .check(jsonPath("$..id").exists.saveAs(ownerIdKey))
+
   val addPet = http("add pet for owner with id ${ownerId}")
     .post("/api/pets")
     .body(StringBody(
@@ -67,31 +83,31 @@ object OwnerActions {
     .check(status.is(201))
 
   def addPet(ownerIdKey: String) = //http(s"add pet for owner with id $${${ownerIdKey}}")
-  http(s"add pet for owner with id ${toEL(ownerIdKey)}")
-    .post("/api/pets")
-    .body(StringBody(
-      s"""
-        |{
-        |  "id":null,
-        |  "owner":{
-        |    "id":${toEL(ownerIdKey)},
-        |    "firstName":"Andrzej",
-        |    "lastName":"Ludwikowski",
-        |    "address":"address",
-        |    "city":"Wrocław",
-        |    "telephone":"123123123",
-        |    "pets":[
-        |
-        |    ]
-        |  },
-        |  "name":"rex",
-        |  "birthDate":"2019\\/09\\/05",
-        |  "type":{
-        |    "id":2,
-        |    "name":"dog"
-        |  }
-        |}
-        |""".stripMargin)).asJson
-    .check(status.is(201))
+    http(s"add pet for owner with id ${toEL(ownerIdKey)}")
+      .post("/api/pets")
+      .body(StringBody(
+        s"""
+           |{
+           |  "id":null,
+           |  "owner":{
+           |    "id":${toEL(ownerIdKey)},
+           |    "firstName":"Andrzej",
+           |    "lastName":"Ludwikowski",
+           |    "address":"address",
+           |    "city":"Wrocław",
+           |    "telephone":"123123123",
+           |    "pets":[
+           |
+           |    ]
+           |  },
+           |  "name":"rex",
+           |  "birthDate":"2019\\/09\\/05",
+           |  "type":{
+           |    "id":2,
+           |    "name":"dog"
+           |  }
+           |}
+           |""".stripMargin)).asJson
+      .check(status.is(201))
 
 }

5c4c04fc041036f606cefbd5c5ea76b21d3d0fad
Andrzej Ludwikowski <andrzejludwikowski@gmail.com>
2019-09-25 17:06:07 +0200

    hacon config

--- src/test/scala/info/ludwikowski/base/PetClinicSimulation.scala
+++ src/test/scala/info/ludwikowski/base/PetClinicSimulation.scala
@@ -1,12 +1,17 @@
 package info.ludwikowski.base
 
+import com.typesafe.config.{Config, ConfigFactory}
 import io.gatling.core.Predef.{Simulation, _}
 import io.gatling.http.Predef.http
 
 class PetClinicSimulation extends Simulation {
 
+  private val conf: Config = ConfigFactory.load
+
+  private val baseUrl = conf.getString("simulation.url")
+
   val httpProtocol = http
-    .baseUrl("http://localhost:9966/petclinic") // Here is the root for all relative URLs
+    .baseUrl(baseUrl) // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
